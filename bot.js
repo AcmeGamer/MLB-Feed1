ffd7ef4e5778f74f4492428dfdb5ffe8608b6a3b
AcmeGamers <52010365+AcmeGamers@users.noreply.github.com>
2020-07-22 13:55:54 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,41 +10,33 @@ client.on('message', msg => {
     msg.reply('Hi there! :D')
   }
 });
-/*  
-  if (msg.content === 'ping') {
-    msg.reply('pong');}
-	 
-  if (msg.content === 'Hi there'||'hi there') {
-    msg.reply('How are you');}
 	
-  if (msg.content === 'I\'m fine'||'I am fine.') {
-    msg.reply('Glad to hear ^^')
-  
-  });
-/*
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-  }
+var TwitterPackage = require('twitter');
+var request = require('request');
+
+console.log("Hello World! I am a twitter bot!");
+
+var secret = {
+client_secret: 'IU2nhDwPrrc3SyNcrGPSsc-QxY_HqlgI',
+access_token_key: 'NzM0OTc4NzUyMzkxNTQ0ODMy.Xxf-lQ.cgciDUWAxLRmuP2E1pYwwsVxZy8',
+}
+
+var Twitter = new TwitterPackage(secret);
+
+//Twitter stream
+var hashtag = '#brexit'; //put any hashtag to listen e.g. #brexit
+console.log('Listening to:' + hashtag);
+
+Twitter.stream('statuses/filter', {track: hashtag}, function(stream) {
+stream.on('data', function(tweet) {
+console.log('Tweet:@' + tweet.user.screen_name + 
+'\t' + tweet.text);
+console.log('------') 
 });
 
-client.on('message', msg => {
-  if (msg.content === 'Hi there'||'hi there') {
-    msg.reply('How are you');
-  }
+stream.on('error', function(error) {
+console.log(error);
+});
 });
 
-client.on('message', msg => {
-  if (msg.content === 'I\'m fine'||'I am fine.') {
-    msg.reply('Glad to hear ^^');
-  }
-});
-
-/*client.on('message', msg => {
-  if (msg.content === 'boop' || 'Boop') {
-    msg.reply('Boops back, ^^');
-  }
-});*/
-	
-	
 client.login(process.env.BOT_TOKEN);
